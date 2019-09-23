b27b007441fbab011cb404ea78e760a5d2f67d5c
spencer.lawry <w58z387@bz-cs-cslab5l.msu.montana.edu>
2019-09-23 15:57:43 -0600

    updated server.py and made bash script: make_board.sh

---
+++ Program1/make_board.sh
@@ -0,0 +1,5 @@
+# run with ./make_board.sh
+# you need execution permission for the file: chmod u+x make_board.sh
+
+python make_new_board.py 0
+cp own_board_copy.html own_board.html
--- Program1/server.py
+++ Program1/server.py
@@ -22,6 +22,8 @@ else:
     for row in board_mat:
         print(row)
 
+    print (board_mat[2][2])
+
 #runs the server indefinitely
 def run():
     try:
@@ -46,7 +48,7 @@ def is_num(n):
 def check_message(x,y):
     if not 0 <= x < 10 or not 0 <= y < 10:
         return 404
-    elif already_shot():
+    elif already_shot(x,y):
         return 410
     else:
         return 200
@@ -54,9 +56,48 @@ def check_message(x,y):
 
 #checks the board for the spots already fired at and returns True if it has already been shot
 # otherwise, it returns False
-def already_shot():
+def already_shot(x,y):
+    fire_spot = board_mat[y][x] #our matrix is column major
+    if fire_spot == 'x':
+        return True
     return False
-# (needs to be implemented)
+
+# "fires" at the targeted x,y location on own_board
+# returns Tuple of (hit, sink) 
+# hit: (hit = 0 or 1) --- 0 means miss, 1 means hit
+# sink: (sink = 0,C,B,R,S or D) --- 0 means no sink, C = carrier(len of ship=5), B = battleship(4), R = cruiser(3), S = submarine(3), D = destroyer(2)
+def fire(x,y):
+    fire_spot = board_mat[y][x] #our matrix is column major
+    board_mat[y][x] = 'x'  # removes the spot from the matrix by replacing it with an 'x'
+    update_file()
+    if fire_spot == '_':
+        return (0, 0) #hit = 0, sink = 0 (none)
+    else:
+        sink = check_sink(fire_spot)
+        return (1, sink)
+
+# updates the file from the board_mat global variable by overwritting it with the current board
+# returns null
+def update_file():
+    with open(sys.argv[2], "w") as board_file:
+        for i,row in enumerate(board_mat):
+            if i > 0:
+                board_file.write("\n")
+            for spot in row:
+                board_file.write(spot)
+
+    for row in board_mat:
+        print(row)
+
+#checks if the hit sank the ship.
+# returns (sink = 0,C,B,R,S or D) --- 0 means no sink, C = carrier, B = battleship, R = cruiser, S = submarine, D = destroyer
+def check_sink(fire_spot):
+    sink = fire_spot   # sink is true given by the char that represents that ship type: (C,B,R,S,D), unless the updated file has that char in it
+    for row in board_mat:
+        for spot in board_mat:
+            if spot == fire_spot:
+                sink = 0
+    return sink
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_POST(self):
@@ -71,9 +112,12 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
         else:
             #print("x:{} y:{}".format(x_val,y_val))
-            self.send_response(400) #bad request
+            mess_type = 400
+            self.send_response(mess_type) #bad request
             self.end_headers()
 
+        if mess_type == 200:
+            fire(x_val, y_val)
 
 
 
